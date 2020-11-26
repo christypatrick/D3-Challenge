3d497c9fb917e84d64c43603f779e362e6285c1b
Christy Patrick <christy06p@msn.com>
2020-11-25 21:48:10 -0600

    updated code with state abbreviations

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,7 +1,7 @@
 // set the dimensions and margins of the graph
 var margin = {top: 10, right: 30, bottom: 50, left: 60},
-    width = 600 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+    width = 700 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 var svg = d3.select("#scatter")
@@ -39,9 +39,25 @@ d3.csv("./assets/data/data.csv").then(function(data) {
     .append("circle")
         .attr("cx", function (d) { return x(d.poverty); } )
         .attr("cy", function (d) { return y(d.healthcare); } )
-        .attr("r", 6)
-        .style("fill", "#407294");
+        .attr("r", 12)
+        .style("fill", "steelblue")
+        .attr("opacity", "0.8");
 
+    // Add the text attributes (state abbreviation)
+    svg.append("g")
+    .selectAll()
+    .data(data)
+    .enter()
+    .append("text")
+        .attr("x", function(d) { return x(d.poverty); })
+        .attr("y", function(d) { return y(d.healthcare); })
+        .text( function (d) { return d.abbr; })
+        .attr("font-family", "sans-serif")
+        .attr("text-anchor", "middle")
+        .attr("font-size", "10px")
+        .attr("fill", "white");
+
+       
     // Add X axis label:
     svg.append("text")
     .attr("text-anchor", "middle")
@@ -58,8 +74,4 @@ d3.csv("./assets/data/data.csv").then(function(data) {
     .attr("dy", "1em")
     .text("Lacks Healthcare (%)")
 
-})
- 
-
-// healthcare(y axis) vs poverty (x axis - 4th)
-// id,state,abbr,poverty,povertyMoe,age,ageMoe,income,incomeMoe,healthcare
+});
